a943a7d0efe3ab0b359e044523c767006758e7f3
olili27 <timmolili27@gmail.com>
2024-10-20 23:10:14 +0300

    read the hours, minutes, seconds from the set new timer container

--- index.js
+++ index.js
@@ -15,29 +15,58 @@ function deleteTimer(deleteButton, noTimerElement) {
     } 
 }
 
+function getNewTimerDetails() {
+    let hours = document.getElementById("hours").textContent;
+    let minutes = document.getElementById("minutes").textContent;
+    let seconds = document.getElementById("seconds").textContent;
+
+    return {hours, minutes, seconds};
+}
+
+function onSpanValueChange(span, defaultValue) {
+    let textContent = parseInt(span.textContent.trim());
+
+    if(isNaN(textContent)) {
+        span.textContent = defaultValue
+        return
+    }
+
+    let id = span.id;
+console.log(id);
+
+    if(id == "hours") {
+        span.textContent = Math.min(textContent, 23)
+    } else {
+        span.textContent = Math.min(textContent, 59);
+    }
+}
+
 function createCurrentTimerContainer() {
     let timerDiv = document.createElement("div");
     timerDiv.classList.add("timer-container");
     timerDiv.classList.add("new-timer");
 
+    let {hours, minutes, seconds} = getNewTimerDetails()
+    console.log(hours, minutes, seconds);
+    
     let divWordsWrapper = document.createElement("div");
     divWordsWrapper.classList.add("words");
 
     let timeLeftSPan = createSpanWithTextContent("Time Left:");
     divWordsWrapper.appendChild(timeLeftSPan);
 
-    let hoursSPan = createSpanWithTextContent("hh")
+    let hoursSPan = createSpanWithTextContent(hours)
     divWordsWrapper.appendChild(hoursSPan);
     divWordsWrapper.r
     let colonSpan = createSpanWithTextContent(":")
     let colonSpan2 = colonSpan.cloneNode(true);
     divWordsWrapper.appendChild(colonSpan)
 
-    let minutesSPan = createSpanWithTextContent("mm");
+    let minutesSPan = createSpanWithTextContent(minutes);
     divWordsWrapper.appendChild(minutesSPan);
     divWordsWrapper.appendChild(colonSpan2);
 
-    let secondsSPan = createSpanWithTextContent("ss");
+    let secondsSPan = createSpanWithTextContent(seconds);
     divWordsWrapper.appendChild(secondsSPan);
 
     let timerIsUp = document.createElement("p");
@@ -80,7 +109,21 @@ function createTimer() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("loaded");
+    let hoursSpan = document.getElementById("hours");
+    let minutesSpan = document.getElementById("minutes");
+    let secondsSpan = document.getElementById("seconds");
+
+    hoursSpan.onblur = function() {
+        onSpanValueChange(hoursSpan, "hh")
+    }
+
+    minutesSpan.onblur = function () {
+      onSpanValueChange(minutesSpan, "mm");
+    };
+
+    secondsSpan.onblur = function () {
+      onSpanValueChange(secondsSpan, "ss");
+    };
 
     let setTimerButton = document.getElementById("set");
     setTimerButton.onclick = createTimer
